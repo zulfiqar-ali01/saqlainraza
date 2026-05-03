09d6c038af8acb15b205351abfc20afb8c2f5c44
Zulfiqar-Ali <zulfibalti592@gmail.com>
2026-05-04 00:04:01 +0500

    multiple section improvement

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_3d8MTPMx_FCUmwe7Z91bC18mHiF8nUyt2");
 
 export async function POST(req: NextRequest) {
   const { name, email, message } = await req.json();
